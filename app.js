65698767d0da3ef16534b4ffab907327376b2940
白洋 <you@example.com>
2018-01-21 02:00:54 +0800

    结构化了文件，将不同类型分离储存

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ class nedis{
         this.set=set;
     }
     //todo 待定
-    save(){}
-    bgsave(){}
-    config(){}
+    // save(){}
+    // bgsave(){}
+    // config(){}
 }
+module.exports.hash=hash.hash;
+module.exports.kv=kv.kv;
+module.exports.list=list.list;
+module.exports.listset=listset.listset;
+module.exports.set=set.set;
 module.exports.cache=nedis;
 module.exports= new nedis();
